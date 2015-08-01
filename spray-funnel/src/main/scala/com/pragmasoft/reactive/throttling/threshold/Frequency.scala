dc4a7c247cb36e36938d75facc11ab9b978371b2
galarragas <galarragas@gmail.com>
2015-08-01 11:51:06 +0100

    Defining dependencies as provided and adding some utility operations to Frequency

--- spray-funnel/src/main/scala/com/pragmasoft/reactive/throttling/threshold/Frequency.scala
+++ spray-funnel/src/main/scala/com/pragmasoft/reactive/throttling/threshold/Frequency.scala
@@ -3,16 +3,22 @@ package com.pragmasoft.reactive.throttling.threshold
 import scala.concurrent.duration._
 
 case class Frequency(amount: Int, interval: FiniteDuration) {
-  override def toString : String = s"$amount every $interval"
+  override def toString: String = s"$amount every $interval"
+
+  def *(factor: Int): Frequency = copy(amount * factor)
+
+  def /(divisor: Int): Frequency = copy(amount / divisor)
 }
 
 trait FrequencyConversions extends Any {
-  protected def forInterval(interval: FiniteDuration) : Frequency
+  protected def forInterval(interval: FiniteDuration): Frequency
 
-  def every(interval: FiniteDuration) : Frequency = forInterval(interval)
+  def every(interval: FiniteDuration): Frequency = forInterval(interval)
 
   def perSecond = every(1 second)
+
   def perMinute = every(1 minute)
+
   def perHour = every(1 hour)
 }
 
